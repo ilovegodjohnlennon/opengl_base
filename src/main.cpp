bdf97dda13f09a7b3d816635011f711adc82c437
barackobama <jlmpghrspb@gmail.com>
2023-03-05 17:39:45 +0300

    First commit. CMake works and links, yay!

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#define GLFW_INCLUDE_NONE
+
+#include <GLFW/glfw3.h>
+#include "../libs/glad/include/glad/gl.h"
+
+#include <iostream>
+#include <glm/glm.hpp>
+
+int main()
+{
+    glfwInit();
+
+    glfwTerminate();
+    
+
+    return 0;
+}
